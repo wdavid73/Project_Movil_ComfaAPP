c5d8862dca263ae1e5d6b048797a31d1c57013af
Wilson Padilla <wpadilla4@cuc.edu.co>
2020-11-25 22:23:56 -0500

    list rentals

--- app/src/main/java/com/example/confmaapp/Activitys/ListRentalsActivity.java
+++ app/src/main/java/com/example/confmaapp/Activitys/ListRentalsActivity.java
@@ -5,12 +5,24 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.confmaapp.Adapters.AdapterRental;
+import com.example.confmaapp.Objects.Data;
+import com.example.confmaapp.Objects.Rental;
 import com.example.confmaapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
 public class ListRentalsActivity extends AppCompatActivity {
+
+    private RecyclerView lst_rentals;
+    private AdapterRental adapterRental;
+    private LinearLayoutManager llm;
+    private ArrayList<Rental> rentals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,17 @@ public class ListRentalsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_rentals);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle(getString(R.string.list_rentals));
+
+        lst_rentals = findViewById(R.id.lstRentals);
+        rentals = Data.get_rentals();
+
+        adapterRental = new AdapterRental(rentals);
+        llm = new LinearLayoutManager(this);
+        llm.setOrientation(RecyclerView.VERTICAL);
+
+        lst_rentals.setLayoutManager(llm);
+        lst_rentals.setAdapter(adapterRental);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/confmaapp/Adapters/AdapterRental.java
@@ -0,0 +1,92 @@
+package com.example.confmaapp.Adapters;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.confmaapp.Objects.Rental;
+import com.example.confmaapp.R;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AdapterRental extends RecyclerView.Adapter<AdapterRental.RentalViewHolder>{
+
+    private ArrayList<Rental> rentals;
+
+    public AdapterRental(ArrayList<Rental> rentals) {
+        this.rentals = rentals;
+    }
+
+    @NonNull
+    @Override
+    public RentalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(
+                parent.getContext()
+        ).inflate(R.layout.item_rental,parent,false);
+        return new RentalViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RentalViewHolder holder, int position) {
+        Rental rental = rentals.get(position);
+
+        Bitmap photo = StringToBitMap(rental.getCloth().getPhoto_cloth());
+        holder.photoClothRental.setImageBitmap(photo);
+        holder.date_now.setText(rental.getDate_now());
+        holder.date_return.setText(rental.getDate_return());
+        holder.price.setText(rental.getPrice());
+        holder.refCloth.setText(rental.getCloth().getRef());
+        holder.sizeCloth.setText(rental.getCloth().getSize());
+        holder.fashionCloth.setText(rental.getCloth().getStyle_fashion());
+        holder.colorCloth.setBackgroundColor(rental.getCloth().getColor());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return rentals.size();
+    }
+
+
+    public static class RentalViewHolder extends RecyclerView.ViewHolder{
+        private CircleImageView photoClothRental;
+        private TextView date_now , date_return , price , refCloth , sizeCloth , fashionCloth;
+        private View colorCloth , v;
+
+
+        public RentalViewHolder(@NonNull View itemView) {
+            super(itemView);
+            v = itemView;
+            photoClothRental = v.findViewById(R.id.imgRentalCloth);
+            date_now = v.findViewById(R.id.lblDateNowRental);
+            date_return = v.findViewById(R.id.lblDateReturnRental);
+            price = v.findViewById(R.id.lblPriceRental);
+            refCloth = v.findViewById(R.id.lblRefClothRental);
+            sizeCloth = v.findViewById(R.id.lblSizeClothRental);
+            fashionCloth = v.findViewById(R.id.lblFashionClothRental);
+            colorCloth = v.findViewById(R.id.vColorClothRental);
+
+        }
+    }
+
+    public Bitmap StringToBitMap(String encodedString){
+        try{
+            byte [] encodeByte = Base64.decode(encodedString,Base64.DEFAULT);
+            return BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+        }
+        catch(Exception e){
+            e.getMessage();
+            return null;
+        }
+    }
+}
